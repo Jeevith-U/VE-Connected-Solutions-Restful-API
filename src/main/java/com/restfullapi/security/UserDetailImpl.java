009429aaeb15e17c1b63af3e5c2515a2561bf5d5
Jeevith-U <jeevith367@gmail.com>
2024-10-07 21:52:38 +0530

    implemented UserDetails and UserDetailService

---
+++ src/main/java/com/restfullapi/security/UserDetailImpl.java
@@ -0,0 +1,30 @@
+package com.restfullapi.security;
+
+import com.restfullapi.entity.Users;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class UserDetailImpl implements UserDetails {
+
+    private final Users user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
---
+++ src/main/java/com/restfullapi/security/UserDetailServiceImpl.java
@@ -0,0 +1,21 @@
+package com.restfullapi.security;
+
+import com.restfullapi.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).map(UserDetailImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Authentication Failed."));
+    }
+}
