c6e38c95ab69082d817a0ac8e691cc7cd8842084
Jeevith-U <jeevith367@gmail.com>
2024-10-07 21:57:00 +0530

    created a bean of type AuthenticationManager

--- src/main/java/com/restfullapi/security/SecurityConfig.java
+++ src/main/java/com/restfullapi/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.restfullapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,9 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(12);
     }
 
-
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
 }
