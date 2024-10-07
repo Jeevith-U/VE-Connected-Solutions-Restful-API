27201e48989a90efae3241bc8a26d83814558b28
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-10-07 11:52:12 +0530

    created and tested product controller

---
+++ src/main/java/com/restfullapi/controller/ProductController.java
@@ -0,0 +1,60 @@
+package com.restfullapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restfullapi.entity.Product;
+import com.restfullapi.responseStructure.ResponseStructure;
+import com.restfullapi.service.ProductService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+public class ProductController {
+	
+	private ProductService productService ;
+	
+	@Autowired
+	public ProductController(ProductService productService) {
+		this.productService = productService ;
+	}
+	
+	@PostMapping("/product")
+	public ResponseEntity<ResponseStructure<Product>> saveProduct(@RequestBody Product product ){
+		return productService.saveProduct(product) ;
+	}
+	
+	@GetMapping("/product/{id}")
+	public ResponseEntity<ResponseStructure<Product>> findProduct(@PathVariable String id){
+		
+		return productService.findProduct(id) ;
+	}
+	
+	@PutMapping("/product/{id}")
+	public ResponseEntity<ResponseStructure<Product>> updateProduct(@RequestBody Product product, @PathVariable String id){
+		
+		return productService.updateProduct(product, id) ;
+		
+	}
+	
+	@DeleteMapping("/product/{id}")
+	public ResponseEntity<ResponseStructure<Boolean>> deleteProduct(@RequestBody Product product, @PathVariable String id){
+		
+		return productService.deleteProduct(product, id) ;
+	}
+	
+	@GetMapping("/product")
+	public ResponseEntity<ResponseStructure<List<Product>>> findAllProducts(){
+		
+		return productService.findAllProduct() ;
+	}
+}
+
