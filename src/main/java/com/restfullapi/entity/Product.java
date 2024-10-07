4cd5db530f5a4ac414b81cded0ec27f39a8898e0
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-10-07 23:15:21 +0530

    Added input validations for entity

--- src/main/java/com/restfullapi/entity/Product.java
+++ src/main/java/com/restfullapi/entity/Product.java
@@ -9,6 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,12 +28,23 @@ public class Product {
 	
 	@Id
 	@GenrateCustomeProductID
-	private String product_id ;
-	private String name ;
-	private String description ;
-	private double product_price ;
-	private long stocks ;
-	
-	@ManyToMany(mappedBy = "productList")
-	private List<Orders> orderlistList ;
-}
+    private String product_id;
+
+    @NotNull(message = "Name should not be null")
+    @Size(min = 5, max = 20, message = "Name must be between 5 and 20 characters")
+    private String name;
+
+    @NotNull(message = "Description should not be null")
+    @Size(min = 10, message = "Description must be at least 10 characters long")
+    private String description;
+
+    @NotNull(message = "Product price should not be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Product price must be greater than 0")
+    private double product_price;
+
+    @NotNull(message = "Stocks should not be null")
+    @Min(value = 0, message = "Stocks must be a non-negative number")
+    private long stocks;
+
+    @ManyToMany(mappedBy = "productList")
+    private List<Orders> orderlistList;}
--- src/main/java/com/restfullapi/entity/Users.java
+++ src/main/java/com/restfullapi/entity/Users.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,8 +28,13 @@ public class Users {
 	@Id
 	@GenrateCustomeUserID
 	private String user_id ;
+	@NotNull(message = "username is required")
+	@Size(min = 3, max = 25, message = "username should be between 5 and 20 character")
 	private String userName ;
+	@NotNull(message = "password is required")
+	@Size(min = 8, message = "Password must be at least 8 characters")
 	private String password ;
+	@Email(message = "Email should be valid")
 	private String email ;
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
