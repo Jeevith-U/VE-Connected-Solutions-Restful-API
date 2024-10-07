19645889c8da867fef9b1270e63ed3aa3ce4a2f7
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-10-07 13:57:27 +0530

    Completed the Order controller with testing

---
+++ src/main/java/com/restfullapi/controller/OrderController.java
@@ -0,0 +1,57 @@
+package com.restfullapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restfullapi.entity.Orders;
+import com.restfullapi.responseStructure.ResponseStructure;
+import com.restfullapi.service.OrderService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+public class OrderController {
+	
+	private OrderService orderService ;
+	
+	public OrderController(OrderService orderService) {
+		this.orderService = orderService ;
+	}
+	
+	@PostMapping("/orders")
+	public ResponseEntity<ResponseStructure<Orders>> saveOrder(@RequestBody Orders orders){
+		return orderService.saveOrder(orders) ;
+	}
+	
+	@GetMapping("/orders/{id}")
+	public ResponseEntity<ResponseStructure<Orders>> findOrder(@PathVariable String id){
+		
+		return orderService.findOrder(id) ;
+	}
+	
+	@GetMapping("/users/{id}/orders")
+	public ResponseEntity<ResponseStructure<List<Orders>>> findUsersOrder(@PathVariable String id){
+		
+		return orderService.getUsersOrder(id) ;
+	}
+	
+	@PutMapping("/orders/{id}")
+	public ResponseEntity<ResponseStructure<Orders>> updateOder(@RequestBody Orders orders, @PathVariable String id){
+		
+		return orderService.updateOrder(orders, id) ;
+		
+	}
+	
+	@DeleteMapping("/orders/{id}")
+	public ResponseEntity<ResponseStructure<Boolean>> deleteOrder(@RequestBody Orders orders, @PathVariable String id){
+		
+		return orderService.deleteOrder(orders, id) ;
+	}
+}
