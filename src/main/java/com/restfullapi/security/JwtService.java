de525776bd810a87da57af176ec94439020c2b44
Jeevith-U <jeevith367@gmail.com>
2024-10-07 22:11:23 +0530

    added JwtServices and implemeted with encoding and parsing logic

---
+++ src/main/java/com/restfullapi/security/JwtService.java
@@ -0,0 +1,39 @@
+package com.restfullapi.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    private String secret;
+
+    public String generateJwt(String username) {
+        return Jwts.builder()
+                .setClaims(Map.of("username", username))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                .signWith(getSignatureKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    private Key getSignatureKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+
+    public Claims parseJwt(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignatureKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
